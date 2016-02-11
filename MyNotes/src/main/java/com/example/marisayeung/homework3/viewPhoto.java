9a300a6c14adb03c67798063b4ca3817135230e0
marisayeung <marisayeung1@gmail.com>
2016-02-11 00:38:12 -0800

    enabled toolbar menu on all activities

--- MyNotes/src/main/java/com/example/marisayeung/homework3/viewPhoto.java
+++ MyNotes/src/main/java/com/example/marisayeung/homework3/viewPhoto.java
@@ -7,6 +7,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -51,6 +53,21 @@ public class viewPhoto extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_notes_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return MenuHelper.handleOnItemSelected(this, item);
     }
 
 }
